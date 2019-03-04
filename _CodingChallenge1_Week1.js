39009c8acde8fd627a95a460fff2487dcb15d229
amro arafat <amroarafat84@gmail.com>
2019-03-04 09:58:02 -0500

    Correct challenge 1

--- _CodingChallenge1_Week1.js
+++ _CodingChallenge1_Week1.js
@@ -11,10 +11,3 @@ function challenge1(num) {
         return "Input: A number as an integer";
 
 }
-
-challenge.fibonacci = function (n) {
-    if (n < 2) {
-        return n
-    }
-    return challenge.fibonacci(n - 1) + challenge.fibonacci(n - 2)
-};
